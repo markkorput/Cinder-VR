ac75737eee73be4021b5539dcb543ce6db5272ba
mark <mark.korput@gmail.com>
2018-02-16 00:49:29 +0100

    add LibOVR sources to cmake config (and rename OVR_CAPIShim.c to .cpp to fix compile error)

--- proj/cmake/Cinder-VRConfig.cmake
+++ proj/cmake/Cinder-VRConfig.cmake
@@ -4,12 +4,13 @@ if( NOT TARGET Cinder-VR )
 	get_filename_component( local_EXT_PATH "${CMAKE_CURRENT_LIST_DIR}/../../ext" ABSOLUTE )
 	get_filename_component( CINDER_PATH "${CMAKE_CURRENT_LIST_DIR}/../../../.." ABSOLUTE )
 
-	FILE(GLOB local_SOURCES ${local_SOURCE_PATH}/cinder/vr/*.cpp ${local_SOURCE_PATH}/cinder/vr/**/*.cpp)
+	FILE(GLOB local_SOURCES ${local_SOURCE_PATH}/cinder/vr/*.cpp ${local_SOURCE_PATH}/cinder/vr/**/*.cpp ${local_EXT_PATH}/LibOVR/Src/*.c*)
 
 	add_library( Cinder-VR ${local_SOURCES} )
 
 	target_include_directories( Cinder-VR PUBLIC "${local_INCLUDE_PATH}")
 	target_include_directories( Cinder-VR PUBLIC "${local_EXT_PATH}/OpenVR/headers")
+	target_include_directories( Cinder-VR PUBLIC "${local_EXT_PATH}/LibOVR/Include")
 	target_include_directories( Cinder-VR SYSTEM BEFORE PUBLIC "${CINDER_PATH}/include" )
 
 	if( NOT TARGET cinder )
